dbe7c5f20cf95c375c7de10263637d3e00c0e12a
Brent Wasilow <brentwasilow2@gmail.com>
2017-02-13 01:53:10 -0800

    Added Driver class handling early rendering of undecorated music player (mini version).

---
+++ src/main/java/com/btwasilow/musicplayer/Driver.java
@@ -0,0 +1,119 @@
+package com.btwasilow.musicplayer;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+import com.btwasilow.musicplayer.input.InputHandler;
+
+public class Driver extends JFrame implements Runnable {
+	private static final long serialVersionUID = 1L;
+	
+	private Thread thread;
+	private boolean running = false;
+	
+	private InputHandler input;
+	
+	private Graphics2D g;
+
+	public Driver() {
+		// setup routines
+		setupGUI();
+		setupInput();
+		
+		// start the program
+		start();
+	}
+	
+	public void start() {
+		// starts the game thread
+		running = true;
+		
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		double ups = 120.0;
+
+		// game loop using preset update rate with unbounded frame rate
+		while (running) {
+			requestFocus();
+
+			final double ns = 1000000000.0 / ups;
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+
+			if ((System.currentTimeMillis() - timer) > 1000) {
+				timer += 1000;
+				System.out.println(updates + " ups" + " | " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
+		}
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		// get graphics and set antialiasing
+		g = (Graphics2D) bs.getDrawGraphics();	
+	    RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	    g.setRenderingHints(rh);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	public void update() {
+		input.update();
+	}
+	
+	private void setupGUI() {
+		// graphical user interface (GUI) setup
+		setUndecorated(true);
+		setShape(new RoundRectangle2D.Double(0, 0, 350, 150, 15, 15));  // miniplayer = (350x150) | fullplayer = (350x400)
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setMinimumSize(new Dimension(350, 150));
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private void setupInput() {
+		// setup input handling operations
+		input = new InputHandler(this);
+		addMouseMotionListener(input);
+		addMouseListener(input);
+		addKeyListener(input);
+		addFocusListener(input);
+	}
+	
+	public static void main(String[] args) {
+		new Driver();
+	}
+
+}
---
+++ src/main/java/com/btwasilow/musicplayer/input/InputHandler.java
@@ -0,0 +1,99 @@
+package com.btwasilow.musicplayer.input;
+
+import java.awt.Point;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import com.btwasilow.musicplayer.Driver;
+
+public class InputHandler implements MouseListener, FocusListener, MouseMotionListener, KeyListener {
+	public Driver driver; // reference to AWT container of main class
+
+	public Point mouseMovedPosition;
+	public Point mouseClickedPosition;
+
+	private boolean[] keys = new boolean[120]; // boolean array for determining which key was pressed
+	public boolean up; // handles up key
+	public boolean down; // handles down key
+	public boolean left; // handles left key
+	public boolean right; // handles right key
+	public boolean escape; // handles escape key
+	public boolean space; // handles space key
+
+	public InputHandler(Driver driver) {
+		this.driver = driver;
+	}
+
+	public void update() {
+		up = keys[KeyEvent.VK_UP];
+		down = keys[KeyEvent.VK_DOWN];
+		left = keys[KeyEvent.VK_LEFT];
+		right = keys[KeyEvent.VK_RIGHT];
+		escape = keys[KeyEvent.VK_ESCAPE];
+		space = keys[KeyEvent.VK_SPACE];
+	}
+
+	public void keyPressed(KeyEvent arg0) {
+		keys[arg0.getKeyCode()] = true;
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		keys[arg0.getKeyCode()] = false;
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+	}
+
+	public void mouseDragged(MouseEvent arg0) {
+        // get location of Window
+        int thisX = driver.getLocation().x;
+        int thisY = driver.getLocation().y;
+
+        // Determine how much the mouse moved since the initial click
+        int xMoved = (thisX + arg0.getX()) - (thisX + mouseClickedPosition.x);
+        int yMoved = (thisY + arg0.getY()) - (thisY + mouseClickedPosition.y);
+
+        // Move window to this position
+        int X = thisX + xMoved;
+        int Y = thisY + yMoved;
+        driver.setLocation(X, Y);
+	}
+
+	public void mouseMoved(MouseEvent arg0) {
+		mouseMovedPosition = arg0.getPoint();
+	}
+
+	public void focusGained(FocusEvent arg0) {
+
+	}
+
+	public void focusLost(FocusEvent arg0) {
+
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// get position of mouse click and determine parent container to allow movement
+		mouseClickedPosition = arg0.getPoint();
+		driver.getComponentAt(mouseClickedPosition);
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+	}
+
+}
